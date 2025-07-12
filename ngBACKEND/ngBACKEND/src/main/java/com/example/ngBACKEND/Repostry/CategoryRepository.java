5223bc55c31de0d0a0a2812b3d9444aebcf8df73
gavin hemsada <69586429+GavinHemsada@users.noreply.github.com>
2025-07-12 13:52:25 +0530

    Delete ngBACKEND/ngBACKEND directory

--- ngBACKEND/ngBACKEND/src/main/java/com/example/ngBACKEND/Repostry/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ngBACKEND.Repostry;
-
-import com.example.ngBACKEND.Entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-    Category findById(int id);
-}
